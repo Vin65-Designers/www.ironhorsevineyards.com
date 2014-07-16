7ac45950be049c0b8a6134538588dcb8d203c414
Jared Siebert <jared@siebertech.com>
2014-07-16 14:38:41 -0700

    Add misc admin panel styles

--- www/admin/styles.js
+++ www/admin/styles.js
@@ -5,23 +5,24 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.addStylesSet( 'my_styles',
 [
-    // Block Styles
+	// Block Styles
 	{ name : 'Pull Quote: Right' , element : 'p', attributes : { 'class' : 'v65-quoteRight' } },
 	{ name : 'Pull Quote: Left' , element : 'p', attributes : { 'class' : 'v65-quoteLeft' } },
 
-	
+
 	// Object Styles
 	{ name : 'Image Right With Border' , element : 'img' , attributes : { 'class' : 'v65-featureRight' } },
 	{ name : 'Image Right' , element:'img' , attributes : { 'class' : 'v65-right' } },
-		
+
 	{ name : 'Image Left With Border' , element : 'img', attributes : { 'class' :'v65-featureLeft' } },
 	{ name : 'Image Left' , element: 'img' , attributes : { 'class' :'v65-left' } },
-		
-	{ name : 'Border' , element: 'img' , attributes : { 'class' :'v65-border' } },
-	
 
-    // Inline Styles
-    { name : 'Small' , element : 'span' , attributes : { 'class' : 'small' } },
+	{ name : 'Border' , element: 'img' , attributes : { 'class' :'v65-border' } },
+
+
+	// Inline Styles
+	{ name : 'Grey Text' , element : 'span' , attributes : { 'class' : 'grey-text' } },
+	{ name : 'Small' , element : 'span' , attributes : { 'class' : 'small' } },
 	{ name : 'Smaller' , element : 'span' , attributes : { 'class' : 'smaller' } }
-	
+
 ]);
